145a3cd2be976da14026b35ba6c19d3d3bd9454a
leonidas <leonidas200999@hotmail.com>
2020-10-20 16:02:20 -0300

    mostrar afazeres especificos para cada pagina (se for status momstra todos os afazeres)

--- JS/Controller.js
+++ JS/Controller.js
@@ -1,3 +1,5 @@
+var num;
+
 function CreateField(){
     var field = '<form>'+
                 '<label>nome</label><br>' +
@@ -17,6 +19,7 @@ function AddStatistic(){
     var name = document.getElementById("nome-N");
     var date = document.getElementById("date-D");
     var hour = document.getElementById("time-T");
+    var docum = document.title;
 
     let dados = JSON.parse(localStorage.getItem("dadosTarefa"));
 
@@ -26,6 +29,7 @@ function AddStatistic(){
     }
 
     var dadosT = {
+        documD: docum,
         nomeN :name.value,
         dataD : date.value,
         horaH : hour.value
@@ -39,21 +43,39 @@ function AddStatistic(){
 function ShowStatistic(){
     var tarefa = JSON.parse(localStorage.getItem('dadosTarefa'));
     var states = '';
+    
     if(tarefa != null){
+        num = 0;
         tarefa.forEach(tf => {
             var state = '<div>'+
+            '<h1>'+tf.documD+'<h1>'+
             '<h2>nome: '+tf.nomeN+'</h2>' +
             '<h2>dia: '+tf.dataD+'</h2>' + 
             '<h2>Horario: '+tf.horaH+'</h2>'+
+            '<input type="submit" onclick="RemoveAction()"  id="'+num+'" value="Remover tarefa">'+
             '<br><br>'+
             '</div>';
-            
-        states += state;    
-        });
+
+        num++;
+        var localTemp = document.title; 
+        if(document.title == "Status"){
+            states += state;  
+        }
+        else if(localTemp== tf.documD){
+            states += state;    
+        }
+        
+    });
         document.getElementById("setActivite").innerHTML = states;
     }
     else{
         var state = 'Nenhuma tarefa Cadastrada';
         document.getElementById("setActivite").innerHTML = state;
     }
+}
+
+function RemoveAction(){
+    alert("tentando remover");
+  //  var tarefa = JSON.parse(localStorage.getItem('dadosTarefa'));
+  //  localStorage.removeItem(tarefa[num.value]);
 }
